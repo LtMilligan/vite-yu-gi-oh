021bee642ded47b7e2219c92624f1d8fc25028be
LtMilligan <gioele.miscia@gmail.com>
2024-07-31 17:57:34 +0200

    [UPD] Adding select and card in main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,20 +8,29 @@ export default {
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    
+                    <select class="form-select">
+                        <option selected>Alien</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
                 </div>
             </div>
-            <div class="container">
+            <div class="container bg-white">
                 <div class="row">
-                    <div class="col-12">
-
+                    <div class="col-12 bg-dark text-light">
+                        <h2>Found card</h2>
                     </div>
                 </div>
-                <div class="row">
+                <div class="row row-cols-5">
                     <div class="col">
                         <div class="card">
-                            
-                        </div>
+                            <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title"></h5>
+                              <p class="card-text"></p>
+                            </div>
+                          </div>
                     </div>
                 </div>
             </div>
@@ -30,5 +39,13 @@ export default {
 </template>
 <style lang="scss" scoped>
     @use '../styles/partials/variables';
-
+    .form-select {
+        width: 200px;
+    }
+    .bg-orange {
+        background-color: orange;
+    }
+    .bg-white {
+        background-color: white;
+    }
 </style>
