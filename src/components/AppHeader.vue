10d0bff3b937e9a35d58fd77b7f06f80f0143bb6
LtMilligan <gioele.miscia@gmail.com>
2024-07-31 17:19:38 +0200

    [UPD] Adding header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,10 +4,22 @@ export default {
 }
 </script>
 <template>
-    <div>
-        <h1>Ciao</h1>
+    <div class="header">
+        <div class="content d-flex p-3">
+            <img src="../assets/logo_yugioh.png" alt="logo">
+            <h1>Yu-Gi-Oh API</h1>
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
-    @use '../styles/partials/variables'
+@use '../styles/partials/variables';
+.header {
+    .content {
+        img {
+            width: 100px;
+            margin-right: 20px;
+        }
+    }
+}
+    
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,10 +4,31 @@ export default {
 }
 </script>
 <template>
-    <div>
-        <h2>Ciao</h2>
+    <div class="bg-orange">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
-    @use '../styles/partials/variables'
+    @use '../styles/partials/variables';
+
 </style>
